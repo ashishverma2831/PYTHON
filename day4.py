e65ef0329dbf8e26ac784ff3315fc9fcb204067e
ashishverma2831 <ashishverma510207@gmail.com>
2025-11-08 02:00:23 +0530

    day4 Completed

--- day4.py
+++ day4.py
@@ -87,3 +87,246 @@ print(f'{a} / {b} = {a / b:.2f}')
 print(f'{a} % {b} = {a % b}')
 print(f'{a} // {b} = {a // b}')
 print(f'{a} ** {b} = {a ** b}')
+
+
+# Accessing characters in a string
+language = 'Python'
+a,b,c,d,e,f = language # unpacking sequence characters into variables
+print(a) # P
+print(b) # y
+print(c) # t
+print(d) # h
+print(e) # o
+print(f) # n
+
+language = 'Python'
+first_letter = language[0]
+print(first_letter) # P
+second_letter = language[1]
+print(second_letter) # y
+last_index = len(language) - 1
+last_letter = language[last_index]
+print(last_letter) # n
+
+# Slicing the string
+language = 'Python'
+first_three = language[0:3] # starts at zero index and up to 3 but not include 3
+print(first_three) #Pyt
+last_three = language[3:6]
+print(last_three) # hon
+# Another way
+last_three = language[-3:]
+print(last_three)   # hon
+last_three = language[3:]
+print(last_three)   # hon
+
+# Reversing a string
+greeting = 'Hello, World!'
+print(greeting[::-1]) # !dlroW ,olleH
+
+# Skipping characters while slicing
+language = 'Python'
+pto = language[0:6:2] #
+print(pto) # Pto
+
+# Converting cases in a string
+challenge = 'thirty days of python'
+print(challenge.capitalize()) # 'Thirty days of python'
+
+# Counting occurrences of a substring
+challenge = 'thirty days of python'
+print(challenge.count('y')) # 3
+print(challenge.count('y', 7, 14)) # 1, 
+print(challenge.count('th')) # 2`
+
+# Checking endswith in a string
+challenge = 'thirty days of python'
+print(challenge.endswith('on'))   # True
+print(challenge.endswith('tion')) # False
+
+# expandtabs() 
+challenge = 'thirty\tdays\tof\tpython'
+print(challenge.expandtabs())   # 'thirty  days    of      python'
+print(challenge.expandtabs(10)) # 'thirty    days      of        python'
+
+# find(): Returns the index of the first occurrence of a substring, if not found returns -1
+challenge = 'thirty days of python'
+print(challenge.find('y'))  # 5
+print(challenge.find('th')) # 0
+
+# rfind(): Returns the index of the last occurrence of a substring, if not found returns -1
+challenge = 'thirty days of python'
+print(challenge.rfind('y'))  # 16
+print(challenge.rfind('th')) # 17
+
+# format(): Formats specified values in a string
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+age = 250
+job = 'teacher'
+country = 'Finland'
+sentence = 'I am {} {}. I am a {}. I am {} years old. I live in {}.'.format(first_name, last_name, age, job, country)
+print(sentence) # I am Asabeneh Yetayeh. I am 250 years old. I am a teacher. I live in Finland.
+
+# Formatted string literals (f-strings)
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+result = 'The area of a circle with radius {} is {}'.format(str(radius), str(area))
+print(result) # The area of a circle with radius 10 is 314
+
+# index(): Returns the index of the first occurrence of a substring, if not found raises an error
+challenge = 'thirty days of python'
+sub_string = 'da'
+print(challenge.index(sub_string))  # 7
+# print(challenge.index(sub_string, 9)) # error
+
+# rindex(): Returns the index of the last occurrence of a substring, if not found raises an error
+challenge = 'thirty days of python'
+sub_string = 'da'
+print(challenge.rindex(sub_string))  # 7
+# print(challenge.rindex(sub_string, 9)) # error
+print(challenge.rindex('on', 8)) # 19
+
+# isalnum(): Checks alphanumeric character
+challenge = 'ThirtyDaysPython'
+print(challenge.isalnum()) # True
+
+challenge = '30DaysPython'
+print(challenge.isalnum()) # True
+
+challenge = 'thirty days of python'
+print(challenge.isalnum()) # False, space is not an alphanumeric character
+
+challenge = 'thirty days of python 2019'
+print(challenge.isalnum()) # False
+
+# isalpha(): Checks alphabetic character
+challenge = 'thirty days of python'
+print(challenge.isalpha()) # False, space is once again excluded
+challenge = 'ThirtyDaysPython'
+print(challenge.isalpha()) # True
+num = '123'
+print(num.isalpha())      # False
+
+# isdecimal(): Checks decimal character
+challenge = 'thirty days of python'
+print(challenge.isdecimal())  # False
+challenge = '123'
+print(challenge.isdecimal())  # True
+challenge = '\u00B2'
+print(challenge.isdigit())   # False
+challenge = '12 3'
+print(challenge.isdecimal())  # False, space not allowed
+
+# isdigit(): Checks digit character
+challenge = 'Thirty'
+print(challenge.isdigit()) # False
+challenge = '30'
+print(challenge.isdigit())   # True
+challenge = '\u00B2'
+print(challenge.isdigit())   # True
+
+# isnumeric(): Checks if all characters in a string are numbers or number related (just like isdigit(), just accepts more symbols, like ½)
+num = '10'
+print(num.isnumeric()) # True
+num = '\u00BD' # ½
+print(num.isnumeric()) # True
+num = '10.5'
+print(num.isnumeric()) # False
+
+# isidentifier(): Checks if the string is a valid identifier
+challenge = '30DaysOfPython'
+print(challenge.isidentifier()) # False, because it starts with a number
+challenge = 'thirty_days_of_python'
+print(challenge.isidentifier()) # True
+
+# islower(): Checks if all characters in the string are lowercase
+challenge = 'thirty days of python'
+print(challenge.islower()) # True
+challenge = 'Thirty days of python'
+print(challenge.islower()) # False
+
+# isupper(): Checks if all characters in the string are uppercase
+challenge = 'thirty days of python'
+print(challenge.isupper()) #  False
+challenge = 'THIRTY DAYS OF PYTHON'
+print(challenge.isupper()) # True
+
+# join(): Joins the elements of an iterable to the end of the string
+web_tech = ['HTML', 'CSS', 'JavaScript', 'React']
+result = ' '.join(web_tech)
+print(result) # 'HTML CSS JavaScript React'
+
+# strip(): Removes all given characters starting from the beginning and the end of the string
+challenge = 'thirty days of pythoonnn'
+print(challenge.strip('noth')) # 'irty days of py'
+
+# replace(): Replaces a substring with another substring
+challenge = 'thirty days of python'
+print(challenge.replace('python', 'coding')) # 'thirty days of coding'
+
+# split(): Splits the string, using a specified separator
+challenge = 'thirty days of python'
+print(challenge.split()) # ['thirty', 'days', 'of', 'python']
+challenge = 'thirty, days, of, python'
+print(challenge.split(', ')) # ['thirty', 'days', 'of', 'python']
+
+# title(): Returns a title cased string
+challenge = 'thirty days of python'
+print(challenge.title()) # Thirty Days Of Python
+
+# swapcase(): Swaps cases, lower case becomes upper case and vice versa
+challenge = 'thirty days of python'
+print(challenge.swapcase())   # THIRTY DAYS OF PYTHON
+challenge = 'Thirty Days Of Python'
+print(challenge.swapcase())  # tHIRTY dAYS oF pYTHON
+
+# startswith(): Checks if the string starts with the given substring
+challenge = 'thirty days of python'
+print(challenge.startswith('thirty')) # True
+
+challenge = '30 days of python'
+print(challenge.startswith('thirty')) # False
+
+
+
+# Exercises
+# 1. Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+result = ' '.join(['Thirty', 'Days', 'Of', 'Python'])
+print(result)
+
+# 2. Declare a variable named company and assign it to an initial value "Coding For All".
+company = "Coding For All"
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+# 3. Split the string 'Coding For All' using space as the separator (split()) 
+print(company.split())
+
+company = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(company.split(', '))
+
+libraries = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+print('# '.join(libraries))
+
+
+radius = 10
+area = 3.14 * radius ** 2
+# The area of a circle with radius 10 is 314 meters square.
+print(f'The area of a circle with radius {radius} is {area} meters square.')
+
+a = 8
+b = 6
+print(f'{a} + {b} = {a + b}')
+print(f'{a} - {b} = {a - b}')   
+print(f'{a} * {b} = {a * b}')
+print(f'{a} / {b} = {a / b:.2f}')
+print(f'{a} % {b} = {a % b}')
+print(f'{a} // {b} = {a // b}')
+print(f'{a} ** {b} = {a ** b}')
